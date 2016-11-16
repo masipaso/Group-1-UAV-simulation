43cc626c6e0044cf4e43eec7183d06c9d3ec1e9a
Dominik Schröck <dominik.schroeck@campus.tu-berlin.de>
2016-11-16 20:14:12 +0100

    Random City

--- shape_model/model.py
+++ shape_model/model.py
@@ -5,7 +5,7 @@ from mesa import Model
 
 from shape_model.obstacles import Obstacle
 from shape_model.base_stations import BaseStation
-
+from random import randint
 class WorldModel(Model):
     '''
     Model representing the world
@@ -27,16 +27,15 @@ class WorldModel(Model):
         # Create Obstacles
         for j in range(1, self.height, 5):
             for i in range(1, self.width, 5):
-                for x in range(0, 4, 1):
-                    if x == 0 or x == 3:
-                        for y in range(0, 4, 1):
-                            obstacle = Obstacle(self, (x + i, y + j))
-                            self.grid.place_agent(obstacle, (x + i, y + j))
-                    else:
-                        obstacle = Obstacle(self, (x + i, j))
-                        self.grid.place_agent(obstacle, (x + i, j))
-                        obstacle = Obstacle(self, (x + i, j + 3))
-                        self.grid.place_agent(obstacle, (x + i, j + 3))
+                #for x in range(0, 4, 1):
+                    #self.make_square(i,j)
+                form = randint(1,3)
+                if form == 1:
+                    self.make_l(i,j)
+                if form == 2:
+                    self.make_square(i,j)
+                if form == 3:
+                    self.make_u(i,j)
 
         # Create BaseStations
         for i in range(self.number_of_base_stations):
@@ -52,3 +51,45 @@ class WorldModel(Model):
 
     def step(self):
         pass
+
+    def make_l(self,i,j):
+        obstacle = Obstacle(self,(i,j))
+        self.grid.place_agent(obstacle,(i,j))
+
+        for x in range(1,4,1):
+            obstacle = Obstacle(self, (i, j+x))
+            self.grid.place_agent(obstacle, (i, j+x))
+
+        for y in range(1,4,1):
+            obstacle = Obstacle(self, (i+y, j ))
+            self.grid.place_agent(obstacle, (i+y, j ))
+
+    def make_u(self,i,j):
+        obstacle = Obstacle(self,(i,j))
+        self.grid.place_agent(obstacle,(i,j))
+
+        for x in range(1,4,1):
+            obstacle = Obstacle(self, (i, j+x))
+            self.grid.place_agent(obstacle, (i, j+x))
+            obstacle = Obstacle(self, (i+3, j + x))
+            self.grid.place_agent(obstacle, (i+3, j + x))
+
+        for y in range(1,4,1):
+            obstacle = Obstacle(self, (i+y, j ))
+            self.grid.place_agent(obstacle, (i+y, j ))
+
+    def make_square(self,i,j):
+        for x in range(1,4,1):
+            obstacle = Obstacle(self, (i, j+x))
+            self.grid.place_agent(obstacle, (i, j+x))
+            obstacle = Obstacle(self, (i+3, j + x))
+            self.grid.place_agent(obstacle, (i+3, j + x))
+
+        for y in range(0, 4, 1):
+            obstacle = Obstacle(self, (i + y, j))
+            self.grid.place_agent(obstacle, (i + y, j))
+
+        obstacle = Obstacle(self, (i+1 , j+3))
+        self.grid.place_agent(obstacle, (i+1, j+3))
+        obstacle = Obstacle(self, (i + 2, j + 3))
+        self.grid.place_agent(obstacle, (i + 2, j + 3))
