78db1ada7f9e1f35b33074ff7119d559722900ae
Dominik Schröck <dominik.schroeck@campus.tu-berlin.de>
2016-12-02 17:22:52 +0100

    Added some measures

--- shape_model/model/worldmodel.py
+++ shape_model/model/worldmodel.py
@@ -50,7 +50,8 @@ class WorldModel(Model):
                 "Items (Picked up)": self.compute_number_of_picked_up_items,
                 "Items (Delivered)": self.compute_number_of_delivered_items,
                 "Average Walk Length": self.compute_average_walk_length,
-                "Standard Deviation of Average Walk Lengths": self.compute_standard_deviation_walklenghts,
+                "Standard Deviation of Average Walk Lengths": self.compute_standard_deviation_walklengths,
+                "Walklength Divided by Distance": self.compute_walklength_divided_by_distance,
              }
         )
 
@@ -229,7 +230,7 @@ class WorldModel(Model):
         else: return 0
 
     @staticmethod
-    def compute_standard_deviation_walklenghts(model):
+    def compute_standard_deviation_walklengths(model):
         average_walks = []
 
         for uav in model.schedule.agents_by_type[Uav]:
@@ -237,5 +238,17 @@ class WorldModel(Model):
                 average_walks.append(elem)
         if len(average_walks) > 0:
             return np.std(average_walks)
+        else:
+            return 0
+
+    @staticmethod
+    def compute_walklength_divided_by_distance(model):
+        length_by_distance = []
+
+        for uav in model.schedule.agents_by_type[Uav]:
+            for elem in uav.get_initial_delivery_distance_divided_by_average_walk_length():
+                length_by_distance.append(elem)
+        if len(length_by_distance) > 0:
+            return sum(length_by_distance)/len(length_by_distance)
         else:
             return 0
